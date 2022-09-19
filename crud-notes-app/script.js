214022dddd92ad8a44b23b0709b42bcea8e2847a
Jan Bock <j.g.bock@outlook.com>
2022-09-19 17:55:29 +0100

    adding alert for info button; adding get information function onclick of info button

--- crud-notes-app/script.js
+++ crud-notes-app/script.js
@@ -1,62 +1,3 @@
-// const notesContainer = document.getElementById("notes-container");
-// const addNoteButton = notesContainer.querySelector(".add-note");
-
-// const getNotes = () => {
-//     return JSON.parse(localStorage.getItem("notes") || "[]") //defaults to empty array if its a new session
-// }
-
-// addNoteButton.addEventListener("click", () => addNote());
-
-// const saveNotes = (notes) => {
-//     localStorage.setItem("notes", JSON.stringify(notes)); //stores notes array to localstorage
-// }
-
-// const createNote = (id, noteContent) => {
-//     //create text area to create a new note element
-    
-//     const el = document.createElement("textarea");
-//     el.classList.add("note");
-//     el.value = noteContent;
-//     el.placeholder = "empty note";
-    
-//     //on note content change:
-//     el.addEventListener("change", () => {
-//         updateNote(id, element.value);
-//     })
-    
-//     el.addEventListener("dblclick", () => {
-//         const doDelete = confirm("Confirm deletion")
-//         if (doDelete) deleteNote(id, el);
-//     })
-// }
-
-// getNotes().forEach(note => {
-//     const noteElement = createNote(note.id, note.content)
-//     notesContainer.insertBefore(noteElement, placeholder);
-// })
-
-// const deleteNote = (id, element) => {
-
-// }
-
-// const updateNote = (id, newContent) => {
-
-// }
-
-// const addNote = () => {
-//     const existingNotes = getNotes();
-//     const noteObject = {
-//         id: Math.floor(Math.random * 100000),
-//         content: ""
-//     };
-
-//     const noteElement = createNote(noteObject.id, noteObject.content);
-//     notesContainer.insertBefore(noteElement, addNoteButton);
-
-//     existingNotes.push(noteObject);
-//     saveNotes(existingNotes);
-// }
-
 const notesContainer = document.getElementById("app");
 const addNoteButton = notesContainer.querySelector(".add-note");
 
@@ -71,7 +12,7 @@ function getNotes() {
   return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
 }
 
-function saveNotes(notes) {
+const saveNotes = (notes) => {
   localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
 }
 
@@ -99,7 +40,7 @@ function createNoteElement(id, content) {
   return element;
 }
 
-function addNote() {
+const addNote = () => {
   const notes = getNotes();
   const noteObject = {
     id: Math.floor(Math.random() * 100000),
@@ -113,7 +54,7 @@ function addNote() {
   saveNotes(notes);
 }
 
-function updateNote(id, newContent) {
+const updateNote = (id, newContent) => {
   const notes = getNotes();
   const targetNote = notes.filter((note) => note.id == id)[0];
 
@@ -121,11 +62,13 @@ function updateNote(id, newContent) {
   saveNotes(notes);
 }
 
-function deleteNote(id, element) {
+const deleteNote = (id, element) => {
   const notes = getNotes().filter((note) => note.id != id);
 
   saveNotes(notes);
   notesContainer.removeChild(element);
 }
 
-
+const giveInfo = () => {
+    alert("This is a simple notes application made entirely with VanillaJS, HTML, and CSS. Checkout jan-bock on GitHub.com for more projects!")
+}
